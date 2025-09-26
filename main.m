524ed0334c2e5fe1e841faac601b13605779c462
loveless-savage <titansofalgenia@gmail.com>
2025-09-26 16:31:32 -0600

    wavenumber k added, viewport adjusts relative to w0

--- main.m
+++ main.m
@@ -1,19 +1,20 @@
-global lambda fnum w0 z0 f tau;
+global k lambda fnum w0 z0 f;
 %Ep=sqrt(1e18/2.146e18); % sqrt of intensity
 lambda = 1;%2*pi; % wavelength = 0.8e-6 m
+k = 2*pi/lambda; % wave number
 fnum = 1; % asymptotic cone angle of beam -- < 3 is a pretty wide focus
-w0 = 2*lambda*fnum/pi;%2*pi*3; % beam waist = 3.2e-6 m
+w0 = 2*lambda*fnum/pi; % beam waist = 3.2e-6 m
 z0 = pi/lambda*w0^2; % rayleigh range = 40.2e-6 m
 f = 10*z0; % focal length of parabolic mirror: should be further than z0
-tau = 2*pi*12; % pulse duration in radians = 40e-15 s
+%tau = 2*pi*12; % pulse duration in radians = 40e-15 s
 
 z = 0;%-f; % displacement from focal plane
 t = 0; % time since peak of pulse hits
 
 %% set up grids for observation plane
 N = 41;
-xmin = -2.5*sqrt(1+z^2/z0^2);
-xmax = 2.5*sqrt(1+z^2/z0^2);
+xmin = -1.25*pi*w0*sqrt(1+z^2/z0^2);
+xmax = 1.25*pi*w0*sqrt(1+z^2/z0^2);
 dx = (xmax-xmin)/(N-1);
 xrange = xmin:dx:xmax;
 ymin = xmin;
@@ -23,7 +24,7 @@ yrange = ymin:dy:ymax;
 [x,y] = meshgrid(xrange,yrange);
 
 %% for each point on observation plane, integrate fields on source plane
-%[Ex,Ey,Ez,Bx,By,Bz] = IgnatovskyIntegral(x,y,z,t);
+[Ex,Ey,Ez,Bx,By,Bz] = IgnatovskyIntegral(x,y,z,t);
 %% compare w/ Singh model
 %[Exs,Eys,Ezs,Bxs,Bys,Bzs] = IgnatovskyIntegral(x,y,z,t);
 %% Dr Peatross code for comparison
@@ -31,7 +32,8 @@ addpath("CrossRender")
 rho = sqrt(x.^2 + y.^2);
 phi = atan2(y,x);
 thetaMax = acos((fnum-1)/(fnum+1));
-[Exp,Eyp,Ezp] = arrayfun(@(r,p) EPeatross(z,r,p,fnum,thetaMax),rho,phi);
+%[Exp,Eyp,Ezp] = arrayfun(@(r,p) EPeatross(z,r,p,fnum,thetaMax),rho,phi);
+[Exp,Eyp,Ezp] = CylinderIntegral(x, y, z, t);
 
 %% show result w/ diagnostic plots
 test
--- test.m
+++ test.m
@@ -1,12 +1,14 @@
-close all;
-[Ex,Ey,Ez] = CylinderIntegral(x, y, z, t);
-%[Ex,Ey,Ez] = arrayfun(@(r,p) CylinderIntegral(z,r,p,fnum,thetaMax),rho,phi);
+%close all;
+[Ex,Ey,Ez] = IgnatovskyIntegral(x, y, z, t);
 
 %% show result w/ diagnostic plots
 FieldCrossRender(x, y, z, Ex, Ey, Ez, 1);
-fig = gcf; fig.Name = "CylinderIntegral";
+fig = gcf; fig.Name = "IgnatovskyIntegral";
 FieldCrossRender(x, y, z,Exp,Eyp,Ezp, 2);
-fig = gcf; fig.Name = "EPeatross";
-Exr = Ex-Exp;
-fig = figure(Name="CylinderIntegral - EPeatross");
-surf(x,y,Exr);
+fig = gcf; fig.Name = "CylinderIntegral";
+Exr = abs(Ex-Exp);
+centErr = max(Exr,[],"all")/max(abs(Exp),[],"all");
+fprintf("center error = %d\n",centErr);
+%fig = figure(Name="difference: (CylinderIntegral - EPeatross)");
+%surf(x,y,Exr);
+%zlim([0 centErr*2]);
