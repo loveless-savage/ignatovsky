891a245cee74fdaef6caadb357ffc7ede6e7345d
loveless-savage <titansofalgenia@gmail.com>
2025-10-02 20:36:02 -0600

    error quantification fixed

--- test.m
+++ test.m
@@ -1,4 +1,4 @@
-%close all;
+close all;
 [Ex,Ey,Ez] = IgnatovskyIntegral(x, y, z, t);
 
 %% show result w/ diagnostic plots
@@ -6,7 +6,7 @@ FieldCrossRender(x, y, z, Ex, Ey, Ez, 1);
 fig = gcf; fig.Name = "IgnatovskyIntegral";
 FieldCrossRender(x, y, z,Exp,Eyp,Ezp, 2);
 fig = gcf; fig.Name = "CylinderIntegral";
-Exr = abs(Ex-Exp);
+Exr = abs(Exp)-abs(Ex);
 centErr = max(Exr,[],"all")/max(abs(Exp),[],"all");
 fprintf("center error = %d\n",centErr);
 %fig = figure(Name="difference: (CylinderIntegral - EPeatross)");
