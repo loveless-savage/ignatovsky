dd9ac170d5815c1a7884d572b972b0484b55ab1b
loveless-savage <titansofalgenia@gmail.com>
2025-10-09 16:12:35 -0600

    E basis rotates correctly, but not camera plane

--- test.m
+++ test.m
@@ -2,18 +2,19 @@ close all;
 oap = deg2rad(15);
 
 % off-axis, viewing plane unaltered
-[Exo,Eyo,Ezo] = IgnatovskyIntegral(x,y,z,t,oap,oaphi);
-[Exq,Eyq,Ezq] = rot(Exo,Eyo,Ezo,oap,oaphi);
-
-% off-axis, viewing plane rotated orthogonal to beam
-[xo,yo,zo] = rot(x,y,z,-oap,oaphi);
+[xo,yo,zo] = rot(x,y,z,0,oaphi);
 [Exo,Eyo,Ezo] = IgnatovskyIntegral(xo,yo,zo,t,oap,oaphi);
-[Exr,Eyr,Ezr] = rot(Exo,Eyo,Ezo,oap,oaphi);
+[Exq,Eyq,Ezq] = rot(Exo,Eyo,Ezo,0,oaphi);
 
 % off-axis, viewing plane rotated halfway between
 [xo,yo,zo] = rot(x,y,z,-oap/2,oaphi);
 [Exo,Eyo,Ezo] = IgnatovskyIntegral(xo,yo,zo,t,oap,oaphi);
 [Ex,Ey,Ez] = rot(Exo,Eyo,Ezo,oap/2,oaphi);
+
+% off-axis, viewing plane rotated orthogonal to beam
+[xo,yo,zo] = rot(x,y,z,-oap+0.01,oaphi);
+[Exo,Eyo,Ezo] = IgnatovskyIntegral(xo,yo,zo,t,oap,oaphi);
+[Exr,Eyr,Ezr] = rot(Exo,Eyo,Ezo,oap-0.01,oaphi);
 
 %% show result w/ diagnostic plots
 FieldCrossRender(x, y, zplane,Exp,Eyp,Ezp, 2);
@@ -23,7 +24,7 @@ fig = gcf; fig.Name = "theta=15,plane=0";
 FieldCrossRender(x, y, zplane, Ex, Ey, Ez, 2);
 fig = gcf; fig.Name = "theta=15,plane=7.5";
 FieldCrossRender(x, y, zplane, Exr,Eyr,Ezr, 3);
-fig = gcf; fig.Name = "theta=15,plane=15";
+fig = gcf; fig.Name = "theta=15,plane=14.99";
 %Exr = abs(Exp)-abs(Ex);
 %centErr = max(Exr,[],"all")/max(abs(Exp),[],"all");
 %fprintf("center error = %d\n",centErr);
