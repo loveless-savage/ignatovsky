e7ca95ca32b437a84357b0f7ed4b8fe8f7c488e9
loveless-savage <titansofalgenia@gmail.com>
2026-02-19 14:36:50 -0700

    Singh function renamed

--- Singh.m
+++ Singh.m
@@ -1,28 +1,16 @@
-%% circular gaussian pulse. The golden child of laser foci.
-function [Ex,Ey,Ez,Bx,By,Bz]=Singh(x,y,z,t)
-global Ep z0 tau;
-Ep=sqrt(1e18/2.146e18); % sqrt of intensity
+%% mode 00 of the paraxial wave equation, as derived by Erikson & Singh
+function [Ex,Ey,Ez]=Singh(x,y,z)
+global k z0 w0;
 
-% intermediate constants
+% intermediate values
 Z=z0+i*z; % coherence length w/ z phase
-rho2=x.^2+y.^2; % sideways radius
-R=z+z0*z0./z; % radial distance from focus
-
-% cross-sectional gaussian envelope
-psi=z0./Z.*exp(-rho2./(2*Z));
-% temporal pulse envelope
-env=exp(-(t-z-rho2./(2*R)).^2/tau^2);
-% overall plane behavior
-plane=exp(i*(z-t));
-
-% scalar field strength
-field=Ep.*psi.*env.*plane;
+psi=exp(-k/2*(x.^2+y.^2)./Z); % cross-sectional gaussian envelope
+scale=psi.*(sqrt(8)*z0./Z); % scalar field amplitude
 
 % vector elements
-Ex=real(field);
-Ey=real(x.*y./(2*Z.^2).*field);
-Ez=real(-i*x./Z.*field);
-Bx=1*Ey;
-By=1*Ex;
-Bz=1*real(-i*y./Z.*field);
-
+Ex=scale;
+Ey=scale.*x.*y/2./Z.^2;
+Ez=scale *-i.*x./Z;
+%Bx=1*Ey;
+%By=1*Ex;
+%Bz=scale.*-i*y./Z;
